404943b92bd4747edf96d89453ef797f9cbe20d2
aldidoanta <aldidkurnia@gmail.com>
2019-11-06 19:14:21 +0700

    [NewsHoax] Add logic to update category_id with default value, and delete old categories

--- api/migrations/m191106_043804_add_type_id_to_news_hoax.php
+++ api/migrations/m191106_043804_add_type_id_to_news_hoax.php
@@ -49,15 +49,26 @@ class m191106_043804_add_type_id_to_news_hoax extends CustomMigration
     {
         // Insert new categories for news_hoax
         $this->insertNewCategories();
+
         // Update news_hoax's category value to default value
         $defaultCategoryModel = Category::find()
             ->select('id')
             ->where(['type' => 'newsHoax', 'name' => 'Lainnya'])
             ->asArray()
             ->one();
-        // NewsHoax::updateAll(['category_id' => $defaultCategoryModel['id']]);
-        // Remove old categories that have been moved to type_id
+        $this->update('news_hoax', ['category_id' => $defaultCategoryModel['id']]);
 
+        // Remove old categories that have been moved to type_id
+        $hoaxTypes = include __DIR__ . '/../config/references/hoax_types.php';
+        $hoaxTypeTitleColumn = array_column($hoaxTypes, 'title');
+        $this->delete(
+            'categories',
+            [
+                'and',
+                ['type' => NewsHoax::CATEGORY_TYPE],
+                ['in', 'name', $hoaxTypeTitleColumn]
+            ]
+        );
     }
 
     private function insertNewCategories()
